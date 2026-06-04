03afbc4d196e22b6ddba7dee2f97e640f4940eb3
shadows204 <2064252290@qq.com>
2026-06-05 00:22:28 +0800

    更新:修改了“负魔金苹果”在饱食度满的情况下无法食用的问题

--- src/main/java/com/github/shadows204/tutorialmod/item/ModFoods.java
+++ src/main/java/com/github/shadows204/tutorialmod/item/ModFoods.java
@@ -6,5 +6,5 @@ public class ModFoods {
     public static final FoodProperties CORN = new FoodProperties.Builder().nutrition(3).saturationModifier(1.0f).build();
     public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(1).saturationModifier(0.3f).build();
     public static final FoodProperties CHEESE = new FoodProperties.Builder().nutrition(5).saturationModifier(0.6f).build();
-    public static final FoodProperties CURSED_GOLDEN_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.8f).build();
+    public static final FoodProperties CURSED_GOLDEN_APPLE = new FoodProperties.Builder().nutrition(4).saturationModifier(0.8f).alwaysEdible().build();
 }
